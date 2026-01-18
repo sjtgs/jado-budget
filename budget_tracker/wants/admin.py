7b994eb999647c779295587e88a1afdac63ed673
sjtgs <chipilej@gmail.com>
2026-01-18 20:24:59 +0200

    Created models for the wants app

--- budget_tracker/wants/admin.py
+++ budget_tracker/wants/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import WantCategory, WantTransaction
 
-# Register your models here.
+
+@admin.register(WantCategory)
+class WantCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at')
+    search_fields = ('name',)
+
+
+@admin.register(WantTransaction)
+class WantTransactionAdmin(admin.ModelAdmin):
+    list_display = ('category', 'amount', 'transaction_date', 'reference')
+    list_filter = ('category', 'transaction_date')
+    date_hierarchy = 'transaction_date'
