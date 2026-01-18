d70b3174f0e8e5ae88e0678bbf48bb874fada3ce
sjtgs <chipilej@gmail.com>
2026-01-18 19:59:04 +0200

    Created Django apps for the project and migrated apps

--- budget_tracker/budget_tracker/settings.py
+++ budget_tracker/budget_tracker/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'income',
+    'needs',
+    'wants',
+    'savings',
+    'debts',
+    'investments',
 ]
 
 MIDDLEWARE = [
---
+++ budget_tracker/wants/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class WantsConfig(AppConfig):
+    name = 'wants'
